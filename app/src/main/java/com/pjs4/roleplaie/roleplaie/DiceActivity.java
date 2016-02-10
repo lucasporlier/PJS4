cc43812c6285e51121d9b712f58c12463e6717d2
eericZ <eeric.zhou@gmail.com>
2016-02-10 10:45:40 +0100

    Possibilité de changer le de a lancer

--- app/src/main/java/com/pjs4/roleplaie/roleplaie/DiceActivity.java
+++ app/src/main/java/com/pjs4/roleplaie/roleplaie/DiceActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 /**
@@ -14,22 +18,57 @@ import android.widget.TextView;
  */
 public class DiceActivity extends Activity {
 
+    private int chosenDice = 100;
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i("projet", "lancement de l'activité des dés");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dice_screen);
+
+        // Hardcoded list of dices
+        String listDices[] = {"4", "6", "8", "10", "12", "20", "100"};
+
+        Log.i("projet", "Selected dice" + String.valueOf(chosenDice));
+
+        tv = (TextView) findViewById(R.id.chosen_dice);
+        tv.setText(tv + String.valueOf(chosenDice));
+
+        final ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listDices);
+
+        ListView lv = (ListView) findViewById(R.id.dice_list);
+
+        lv.setAdapter(listAdapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String chosenDiceInList = String.valueOf(parent.getItemAtPosition(position));
+
+                chosenDice = Integer.parseInt(chosenDiceInList);
+
+                Log.i("projet", "Selected dice" + String.valueOf(chosenDice));
+
+                tv.setText(getResources().getText(R.string.you_chose_the_dice) + String.valueOf(chosenDice));
+
+
+            }
+        });
     }
 
     /**
-     * Gives a random result between 1 and the max possible result of the choosen dice
+     * Gives a random result between 1 and the max possible result of the chosen dice
      *
      * @param view
      */
     public void launchDice(View view) {
-        int result = Dice.launchDice(100);
+        Log.i("projet", "preparation du lancer du de : " + String.valueOf(chosenDice));
+        int result = Dice.launchDice(chosenDice);
+
         TextView tv = (TextView) findViewById(R.id.dicePlace);
-        tv.setText(result + "");
+        tv.setText(String.valueOf(result));
+
         Log.i("projet", "Dés lancés");
     }
 }
