da5eee693672ab6e7f11b7560a68b873eb51c537
eericZ <eeric.zhou@gmail.com>
2016-03-07 08:31:10 +0100

    modification mineur de la fiche perso

--- app/src/main/java/com/pjs4/roleplaie/roleplaie/PlayerFileActivity.java
+++ app/src/main/java/com/pjs4/roleplaie/roleplaie/PlayerFileActivity.java
@@ -9,15 +9,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 /**
+ * Used to manage the player's information
  * Created by ZHOU Eric on 22/02/2016.
  */
 public class PlayerFileActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.player_file_layout);
-        Log.i("projet", "OnCreate PlayerFileActivity");
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.player_file_layout);
+		Log.i("projet", "OnCreate PlayerFileActivity");
 
         /*
            fill player informations here:
@@ -31,71 +32,72 @@ public class PlayerFileActivity extends Activity {
            CAPACITIES
         */
 
-        final TextView tvHP = (TextView) findViewById(R.id.tvHP);
-        SeekBar sbHP = (SeekBar) findViewById(R.id.seekBarHP);
-        sbHP.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.i("projet", "onProgressChanged");
+		final TextView tvHP = (TextView) findViewById(R.id.tvHP);
+		SeekBar sbHP = (SeekBar) findViewById(R.id.seekBarHP);
+		sbHP.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				Log.i("projet", "onProgressChanged");
 
-                tvHP.setText(String.valueOf(progress));
-            }
+				tvHP.setText(String.valueOf(progress));
+			}
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+			}
+		});
 
 
-        final TextView tvMP = (TextView) findViewById(R.id.tvMP);
-        SeekBar sbMP = (SeekBar) findViewById(R.id.seekBarMP);
-        sbMP.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.i("projet", "onProgressChanged");
+		final TextView tvMP = (TextView) findViewById(R.id.tvMP);
+		SeekBar sbMP = (SeekBar) findViewById(R.id.seekBarMP);
+		sbMP.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				Log.i("projet", "onProgressChanged");
 
-                tvMP.setText(String.valueOf(progress));
-            }
+				tvMP.setText(String.valueOf(progress));
+			}
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
 
-        final TextView tvXP = (TextView) findViewById(R.id.tvXP);
-        SeekBar sbXP = (SeekBar) findViewById(R.id.seekBarXP);
-        sbXP.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.i("projet", "onProgressChanged");
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+			}
+		});
 
-                tvXP.setText(String.valueOf(progress));
-            }
+		final TextView tvXP = (TextView) findViewById(R.id.tvXP);
+		SeekBar sbXP = (SeekBar) findViewById(R.id.seekBarXP);
+		sbXP.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				Log.i("projet", "onProgressChanged");
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
+				tvXP.setText(String.valueOf(progress));
+			}
 
-    }
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
 
-    public void bleuh(View view) {
-        SeekBar sb = (SeekBar) findViewById(R.id.seekBarHP);
-        sb.setMax(20);
-        sb.setProgress(14);
-    }
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+			}
+		});
 
-    public void inventory(View view) {
-        Toast.makeText(this, R.string.todo, Toast.LENGTH_SHORT).show();
-    }
+	}
+
+	public void inventory(View view) {
+		Toast.makeText(this, R.string.todo, Toast.LENGTH_SHORT).show();
+/*
+		Intent intent = new Intent(this, Inventory.class);
+		// which caracter
+		startActivity(intent);*/
+		
+	}
 }
