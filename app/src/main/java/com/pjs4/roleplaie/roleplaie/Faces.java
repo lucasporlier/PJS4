f1c34f086e43cf9740c516c150d80232f8c74f2c
Lucas Porlier <porlierp@B-0-03-06.iutparis.local>
2016-03-21 12:06:49 +0100

    Creation d une classe pour les differentes faces des dices

---
+++ app/src/main/java/com/pjs4/roleplaie/roleplaie/Faces.java
@@ -0,0 +1,42 @@
+package com.pjs4.roleplaie.roleplaie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Porlier-Pagnon Lucas on 21/03/2016.
+ */
+public class Faces {
+    private static List<List<Integer>> faces;
+
+
+    public Faces() {
+        faces = new ArrayList<>();
+
+        faces.add(new ArrayList<Integer>());
+
+        List<Integer> tmp = faces.get(0);
+
+        tmp.add(R.drawable.face1);
+        tmp.add(R.drawable.face2);
+        tmp.add(R.drawable.face3);
+        tmp.add(R.drawable.face4);
+        tmp.add(R.drawable.face5);
+        tmp.add(R.drawable.face6);
+/*
+        tmp = faces.get(1);
+
+        tmp.add(R.drawable.face1);
+        tmp.add(R.drawable.face2);
+        tmp.add(R.drawable.face3);
+        tmp.add(R.drawable.face4);
+        tmp.add(R.drawable.face5);
+        tmp.add(R.drawable.face6);
+
+*/
+    }
+
+    public static int getFace(int de, int i) {
+        return faces.get(de).get(i);    //TODO pas de gestion d'erreur pour l'instant
+    }
+}
