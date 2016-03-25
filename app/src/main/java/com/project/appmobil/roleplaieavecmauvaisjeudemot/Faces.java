92fde358283c0b332eafcc66329a1d9c8d21888f
Lucas Porlier <porlierp@B-0-03-05.iutparis.local>
2016-03-25 09:56:50 +0100

    recup Layout&Visuel a la mano

---
+++ app/src/main/java/com/project/appmobil/roleplaieavecmauvaisjeudemot/Faces.java
@@ -0,0 +1,42 @@
+package com.project.appmobil.roleplaieavecmauvaisjeudemot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Porlier-Pagnon Lucas on 21/03/2016.
+ */
+public class Faces {
+    private static List<List<Integer>> faces;
+
+
+    public Faces() {
+        faces = new ArrayList<>();
+
+        faces.add(new ArrayList<Integer>());
+
+        List<Integer> tmp = faces.get(0);
+
+        tmp.add(R.drawable.d6f1);
+        tmp.add(R.drawable.d6f2);
+        tmp.add(R.drawable.d6f3);
+        tmp.add(R.drawable.d6f4);
+        tmp.add(R.drawable.d6f5);
+        tmp.add(R.drawable.d6f6);
+/*
+        tmp = faces.get(1);
+
+        tmp.add(R.drawable.face1);
+        tmp.add(R.drawable.face2);
+        tmp.add(R.drawable.face3);
+        tmp.add(R.drawable.face4);
+        tmp.add(R.drawable.face5);
+        tmp.add(R.drawable.face6);
+
+*/
+    }
+
+    public static int getFace(int de, int i) {
+        return faces.get(de).get(i);    //TODO pas de gestion d'erreur pour l'instant
+    }
+}
